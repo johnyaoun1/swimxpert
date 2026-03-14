db80fe2fbd97d8d1c5057c8acba8e367137b2ec4
johnyaoun1 <johnyaoun23@gmail.com>
2026-03-14 21:12:08 +0200

    feat(seo): swim-lessons page title/meta and semantic HTML

--- src/app/pages/swim-lessons/swim-lessons.component.ts
+++ src/app/pages/swim-lessons/swim-lessons.component.ts
@@ -1,6 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
+import { Title, Meta } from '@angular/platform-browser';
 import { SwimLevelsService } from '../../services/swim-levels.service';
 
 @Component({
@@ -10,8 +11,20 @@ import { SwimLevelsService } from '../../services/swim-levels.service';
   templateUrl: './swim-lessons.component.html',
   styleUrls: ['./swim-lessons.component.scss']
 })
-export class SwimLessonsComponent {
+export class SwimLessonsComponent implements OnInit {
   levels = this.swimLevelsService.getLevels();
 
-  constructor(private swimLevelsService: SwimLevelsService) {}
+  constructor(
+    private swimLevelsService: SwimLevelsService,
+    private title: Title,
+    private meta: Meta
+  ) {}
+
+  ngOnInit(): void {
+    this.title.setTitle('Swimming Lessons Lebanon | All Levels | SwimXpert');
+    this.meta.updateTag({
+      name: 'description',
+      content: 'Explore SwimXpert swimming programs for all skill levels in Lebanon. Beginner, intermediate and advanced classes with certified coaches.'
+    });
+  }
 }
